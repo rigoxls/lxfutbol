67d7866d6803bff544d1e523be6dfb1ace24c61f
Rigo <rigoxls@gmail.com>
2020-10-11 20:05:43 -0500

    improving docker-compose

--- integratorService/src/main/java/com/lxfutbol/integrator/service/ProviderProxyService.java
+++ integratorService/src/main/java/com/lxfutbol/integrator/service/ProviderProxyService.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.lxfutbol.integrator.dto.Providerdto;
 
-@FeignClient(name = "provider-service", url = "http://192.168.99.100:8080")
+//Feign sabe que tiene que hablar con Eureka server
+//@FeignClient(name = "provider-service", url = "http://192.168.99.100:8080")
+@FeignClient(name = "provider-service")
 public interface ProviderProxyService {
 
 	@GetMapping("/provider/get/{providerId}")
