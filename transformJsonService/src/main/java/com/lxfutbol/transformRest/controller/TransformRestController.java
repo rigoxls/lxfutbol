1ef795196b6291a6615ff6fd071e5a92a0cc8aff
Rigo <rigoxls@gmail.com>
2020-10-23 23:22:05 -0500

    Json service getting data from central ms

--- transformJsonService/src/main/java/com/lxfutbol/transformRest/controller/TransformRestController.java
+++ transformJsonService/src/main/java/com/lxfutbol/transformRest/controller/TransformRestController.java
@@ -2,24 +2,27 @@ package com.lxfutbol.transformRest.controller;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lxfutbol.transformRest.dto.Providerdto;
 import com.lxfutbol.transformRest.service.ProviderProxyService;
 import com.lxfutbol.transformRest.service.TransformRestService;
 
-//import com.lxfutbol.integratorDTO;
-
-
 @RestController
 public class TransformRestController {
+	
+	private final Logger LOG = LoggerFactory.getLogger(TransformRestController.class);
 
 	@Autowired
 	private TransformRestService integratorService;
-	
+	/*
 	@Autowired
 	private ProviderProxyService providerProxyService;	
 
@@ -27,5 +30,16 @@ public class TransformRestController {
 	public Providerdto getProviderById(@PathVariable long providerId) {
 		Providerdto provider = providerProxyService.getProviderById(providerId);
 		return provider;
-	}
+	}*/
+	
+	@PostMapping("/transform/{idProvider}")
+	public String transfor(@PathVariable int idProvider, @RequestBody String menssage) {
+		
+		LOG.info("*******************************+");
+		LOG.info(String.valueOf(idProvider));
+		LOG.info(menssage);
+		LOG.info("*******************************+");
+		
+		return "lo que retorna a nat";
+	}	
 }
