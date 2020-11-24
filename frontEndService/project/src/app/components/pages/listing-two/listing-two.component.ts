4bed94c1f67307043ed132fd418ed60567ca55c8
Rigo <rigoxls@gmail.com>
2020-11-23 19:39:17 -0500

    ending front page lodge

--- frontEndService/project/src/app/components/pages/listing-two/listing-two.component.ts
+++ frontEndService/project/src/app/components/pages/listing-two/listing-two.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Spectactle} from '../../interfaces/spectacle.interface';
 import {Lodge} from '../../interfaces/lodge.interface';
 import {LodgeService} from './lodge.service';
+import {NgbDate, NgbCalendar, NgbDateParserFormatter} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
     selector: 'app-listing-two',
@@ -11,9 +13,40 @@ import {LodgeService} from './lodge.service';
 export class ListingTwoComponent implements OnInit {
 
     lodges: Lodge[];
+    bkLodges: Lodge[];
 
-    constructor(private lodgeService: LodgeService) {
+    hoveredDate: NgbDate | null = null;
+
+    fromDate: NgbDate | null;
+    toDate: NgbDate | null;
+
+    constructor(private lodgeService: LodgeService, private calendar: NgbCalendar, public formatter: NgbDateParserFormatter) {
         this.getLodges();
+        setTimeout(() => {
+            const lis = document.getElementsByClassName('option');
+            // @ts-ignore
+            for (const li of lis) {
+                li.addEventListener('click', (data) => {
+                    const text = data.target.innerText;
+
+                    switch (text) {
+                        case 'Ordenar: Destacados':
+                            this.sortLodges(1);
+                            break;
+                        case 'Ordenar: Precio mas bajo':
+                            this.sortLodges(2);
+                            break;
+                        case 'Ordenar: Precio mas alto':
+                            this.sortLodges(3);
+                            break;
+                    }
+
+                }, false);
+            }
+
+
+        }, 1000);
+
     }
 
     ngOnInit(): void {
@@ -21,7 +54,51 @@ export class ListingTwoComponent implements OnInit {
 
     private async getLodges(): Promise<Lodge[]> {
         this.lodges = await this.lodgeService.getLodges();
+        this.bkLodges = this.lodges;
         return this.lodges;
+    }
+
+    private sortLodges(sort) {
+        this.lodges = this.bkLodges;
+        if (sort === 1) {
+            return;
+        }
+        this.lodges.sort((a, b) => {
+            if (sort === 2) {
+                return (a.price > b.price) ? 1 : -1;
+            } else {
+                return (a.price > b.price) ? -1 : 1;
+            }
+
+        });
+    }
+
+    onDateSelection(date: NgbDate) {
+        if (!this.fromDate && !this.toDate) {
+            this.fromDate = date;
+        } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+            this.toDate = date;
+        } else {
+            this.toDate = null;
+            this.fromDate = date;
+        }
+    }
+
+    isHovered(date: NgbDate) {
+        return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+    }
+
+    isInside(date: NgbDate) {
+        return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+    }
+
+    isRange(date: NgbDate) {
+        return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+    }
+
+    validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+        const parsed = this.formatter.parse(input);
+        return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
     }
 
 }
