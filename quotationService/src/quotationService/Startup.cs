cfb497133299bf2b11caa7a0e9d0d95d88e75479
rodbastidas <rodrigo.bastidas@tory-tech.com>
2020-11-24 19:04:45 -0500

    ok docker

--- quotationService/src/quotationService/Startup.cs
+++ quotationService/src/quotationService/Startup.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
