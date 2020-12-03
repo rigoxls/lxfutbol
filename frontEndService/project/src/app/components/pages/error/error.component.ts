77ca84c64e1a640ecfd8f0bdafbf0b455d85057c
natalia-Leon <leonc.natalia@gmail.com>
2020-12-02 23:04:12 -0500

    Ajuste creación de ordenes

--- frontEndService/project/src/app/components/pages/error/error.component.ts
+++ frontEndService/project/src/app/components/pages/error/error.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemsInterface } from '../../interfaces/items.interface';
+import { OrderInterface } from '../../interfaces/orders.interface';
 import { Usuario } from '../../interfaces/usuario.interface';
+import { ErrorService } from './error.service';
 
 @Component({
   selector: 'app-error',
@@ -8,11 +11,34 @@ import { Usuario } from '../../interfaces/usuario.interface';
 })
 export class ErrorComponent implements OnInit {
 
-  userPaying: Usuario;
-  constructor() { }
+  orderPayment: any;
+  order: OrderInterface = {} as any;
+  itemsOrder: ItemsInterface[] = [];
+  item: ItemsInterface = {} as any;
+  constructor(private errorService: ErrorService) { }
 
   ngOnInit(): void {
-    this.userPaying =  JSON.parse(localStorage.getItem('userPaying'));
+    this.orderPayment = JSON.parse(localStorage.getItem('OrderPayment'));
+
+    this.order.nameUser = this.orderPayment.name;
+    this.order.lastNameUser = this.orderPayment.lastname;
+    this.order.numDocumentUser = this.orderPayment.numDocumentUser;
+    this.order.totalValue = this.orderPayment.totalOrder;
+    this.order.email = this.orderPayment.email;
+    this.order.address = this.orderPayment.address;
+    this.order.paidStatus = 'Rechazado';
+    this.order.phone = this.orderPayment.phone;
+
+    for (var items in this.orderPayment.selectProviders) {
+      this.item.name = this.orderPayment.selectProviders[items].name;
+      this.item.value = this.orderPayment.selectProviders[items].price;
+      this.item.quantity = this.orderPayment.selectProviders[items].numPeople;
+      this.itemsOrder.push(this.item);
+    }
+
+    this.order.items = this.itemsOrder;
+    console.log(JSON.stringify(this.order));
+    this.errorService.createOrder(this.order);
   }
 
 }
---
+++ frontEndService/project/src/app/components/pages/success/success.service.ts
@@ -0,0 +1,23 @@
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {environment} from 'src/environments/environment';
+import { OrderInterface } from '../../interfaces/orders.interface';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SuccessService {
+    
+    private url = environment.userServiceUrl + 'orders/createOrder';
+    
+    constructor(private http: HttpClient) {
+    }
+
+    public createOrder(order ?: OrderInterface) {
+        const headers = {
+            'Content-Type': 'application/json',
+        };
+        return this.http.post(this.url, order, {headers});
+    }
+
+}
